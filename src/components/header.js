93f25ca1449e52aabcaf4c434deee0f75bb86855
Aryan Zodge <aryanzodge@Aryans-MacBook-Pro.local>
2018-08-06 23:49:33 -0600

    modified size and color

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ const Header = () => {
         <div className="header">
             <div className="header__skew"></div>
             <div className="header__border"></div>
-            <h1>Bottega Madlibs</h1>
-            <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
+            <h1>AZ Madlibs</h1>
+            <p>Type what you want below and click the button to see your story!</p>
         </div>
     )
 }
